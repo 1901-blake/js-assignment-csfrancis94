5b1cf59996b01a5296cb9a57bc911b81499534b9
csfrancis94 <46684034+csfrancis94@users.noreply.github.com>
2019-01-16 08:19:16 -0500

    Add files via upload

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,14 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
+//first way
+for(p in someObj) {
+    let v = someObj[p]
 
+    console.log(p,v);
 }
+//also we can use json stringify
+console.log(JSON.stringify(someObj, null, "    "));
+}
+
+objectProperties({a:0,b:2,c:3,d:{a:2,b:0}});
