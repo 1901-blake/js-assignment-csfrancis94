954ed1690bb17a15e214242f85c34e0c1ca31ab9
csfrancis94 <46684034+csfrancis94@users.noreply.github.com>
2019-01-16 08:20:03 -0500

    Add files via upload

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -3,5 +3,8 @@ Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
 function spliceElement(someArr, index) {
-  
+  someArr.splice(index, 1);
+  return someArr;
 }
+
+console.log(spliceElement([3,2,1], 0));
