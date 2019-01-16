4c7b2de95f622c586eeb3470ee8898084cf1d893
csfrancis94 <46684034+csfrancis94@users.noreply.github.com>
2019-01-15 21:15:56 -0500

    Add files via upload

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,13 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+  for (let i = 0; i < ~~someStr.length/2; i++)
+	  if (!(someStr.charAt(i) == someStr.charAt(someStr.length - i - 1)))
+		  return false;
+  return true;
 }
+
+console.log(isPalindrome("1234321"));
+console.log(isPalindrome("1282"));
+console.log(isPalindrome("0"));
+console.log(isPalindrome("f0f"));
