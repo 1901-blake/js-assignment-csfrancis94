7b5d0ff39abf5601ac3922e66b7e30f5ecf36129
csfrancis94 <46684034+csfrancis94@users.noreply.github.com>
2019-01-15 19:31:54 -0500

    Add files via upload

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -1,6 +1,14 @@
 /* 1. Fibonacci
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
-function fib(n) {
 
+//fib(1) for 1st, fib(2) for 2nd, fib(3) for 3rd...
+function fib(n) {
+let val = 1;
+let a = 1;
+let b = 1;
+for(let i = 0 && n > 2; i < n-2; i++){ val = a + b; a = b; b = val;}
+console.log(val);
 }
+
+fib(8);
