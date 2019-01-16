4720032148ef48fde5a27b809641fc12e29cf92d
csfrancis94 <46684034+csfrancis94@users.noreply.github.com>
2019-01-15 20:17:14 -0500

    Add files via upload

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,13 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+  let newStr = ""
+  for(let i = 0; i < someStr.length; i++)
+  {
+	  newStr += someStr.charAt(someStr.length-i-1);
+  }
+  return newStr;
 }
+
+console.log(reverseStr("veritas"));
+//should print satirev
